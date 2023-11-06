1df766fcb77e6082d7bd590ba7d2dea3f091f81b
Jaime Zepeda Pina <jzepedapina@gmail.com>
2023-11-06 15:04:36 -0600

    Initial Commit

---
+++ Tuner/TunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunerApp.swift
+//  Tuner
+//
+//  Created by Jaime Zepeda on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
